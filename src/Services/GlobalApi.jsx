c851faf551229224b89ec27c2f12be440410ae9c
rrs301 <innocruts@gmail.com>
2023-05-22 09:13:10 -0400

    Part 1

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-const BASE_URL='http://localhost:1337/api';
+// const BASE_URL='http://localhost:1337/api';
+const BASE_URL='https://tubeguruji-admin.herokuapp.com/api'
 
 const getPost=axios.get(BASE_URL+'/blogs?populate=*');
 
